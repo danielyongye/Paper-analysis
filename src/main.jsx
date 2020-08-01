f684990579a4aac5996dbaf46303c9bb105fea7c
徐步欣 <buxin.xu@lazada.com>
2020-08-01 20:58:32 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -7,20 +7,27 @@ class App extends React.Component {
             value: '',
         }
     }
-    onChange = (value) => {
-        console.log('value', value);
-        // this.setState({ value });
+    onChange = (e) => {
+        console.log('value', e.target.value);
+        this.setState({ value: e.target.value });
     }
     render() {
         const { value } = this.state;
         return (<div style={styles.page}>
-            <label htmlFor="name">App containers</label>
-            <input type="text"/>
+            <div>
+                <label htmlFor="name">App containers</label>
+                <textarea
+                    // ref={(textarea) => this.textarea = textarea}
+                    value={this.state.value}
+                    onChange={this.onChange} />
+                {/* <input type="text" onChange={this.onChange} /> */}
+            </div>
+            <div>{this.state.value}</div>
         </div>);
     }
 }
-const styles={
-    page:{
+const styles = {
+    page: {
         margin: '20px auto 0',
         textAlign: 'center',
     }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const IS_PROD = process.env.NODE_ENV === 'production';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const uglify = require('uglifyjs-webpack-plugin');
 
 const BUILD_DIST = 'dist';
 
@@ -12,7 +14,7 @@ module.exports = {
         path: path.join(__dirname, BUILD_DIST), // 定义输出目录
         filename: 'bundle.js'  // 定义输出文件名称
     },
-    devtool: 'cheap-eval-source-map',
+    devtool: IS_PROD ? undefined : 'cheap-eval-source-map',
     devServer: {
         contentBase: './dist',
     },
@@ -69,9 +71,22 @@ module.exports = {
     },
     resolve: {
         extensions: ['.jsx', '.mjs', '.js', '.ts', '.tsx', '.json'],
-        symlinks: false, // support npm link with peerDependences
     },
     plugins: [
+        // new CleanWebpackPlugin([BUILD_DIST]),
+        // new uglify(),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: `index.css`,
+            chunkFilename: `index.css`,
+            minify: {
+                // 压缩HTML⽂件
+                removeComments: true, // 移除HTML中的注释
+                collapseWhitespace: true, // 删除空⽩符与换⾏符
+                minifyCSS: true // 压缩内联css
+            },
+        }),
         new HtmlWebpackPlugin({
             template: './index.html',
             title: 'Paper analysis',
